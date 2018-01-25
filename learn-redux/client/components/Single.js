1d6345f48ecbfa6bc0a291d38ff6d55a7ab1a39e
Tiago Garcia <tiago.garcia@macys.com>
2018-01-24 22:21:26 -0800

    more wip

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 export default class Single extends React.Component {
   render() {
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex(post => post.code === postId);
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className="single-photo">
-        Single<hr />
+        <Photo {...this.props} key={i} i={i} post={post} />
+        <Comments {...this.props} postComments={postComments} />
       </div>
     );
   }
--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,6 +1,27 @@
+const postComments = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      return [
+        ...state,
+        {
+          user: action.author,
+          text: action.comment
+        }
+      ];
+    case 'REMOVE_COMMENT':
+      return [...state.slice(0, action.i), ...state.slice(action.i + 1)];
+    default:
+      return state;
+  }
+};
+
 const comments = (state = [], action) => {
-  console.log('The comments will change');
-  console.log(state, action);
+  if (typeof action.postId !== 'undefined') {
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action)
+    };
+  }
   return state;
 };
 
